5fa6a5518071a226773a69f127d3bed68228aab9
Miguel C <webdazer0@gmail.com>
2021-04-19 16:15:22 +0200

    1: http, get data from server

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -16,6 +19,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  String baseUrl = 'https://librojs.herokuapp.com/api/books';
+
+  getBooks() async {
+    http.Response response = await http.get(Uri.parse(baseUrl));
+    debugPrint(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getBooks();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
